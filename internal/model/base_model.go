95008dce5136debd1b8af855a7aef214481f2c4f
teslapatrick <teslapatrick@gmail.com>
2024-04-07 12:22:46 +0800

    update: generate uuid before create

--- internal/model/base_model.go
+++ internal/model/base_model.go
@@ -1,17 +1,27 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 // Model is the base model, like gorm.Model
 type Model struct {
-	ID        uint64     `gorm:"column:id;AUTO_INCREMENT;primary_key" json:"id"`
+	ID        string     `gorm:"type:string;primary_key" json:"id"`
 	CreatedAt time.Time  `gorm:"column:created_at" json:"createdAt"`
 	UpdatedAt time.Time  `gorm:"column:updated_at" json:"updatedAt"`
-	DeletedAt *time.Time `gorm:"column:deleted_at" json:"deletedAt"`
+	DeletedAt *time.Time `gorm:"column:deleted_at" json:"-"`
 }
 
 type BaseChain struct {
 	Address   string `gorm:"column:address;type:varchar(66);NOT NULL" json:"address"`
 	ChainID   uint64 `gorm:"column:chain_id;type:int(11);NOT NULL" json:"chainId"`
 	ChainName string `gorm:"column:chain_name;type:varchar(50);NOT NULL" json:"chainName"`
+}
+
+func (m *Model) BeforeCreate(tx *gorm.DB) error {
+	m.ID = uuid.New().String()
+	return nil
 }
