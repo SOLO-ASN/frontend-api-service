750c78cd297d4991e7461f7e23f6064ea8bb59d8
teslapatrick <teslapatrick@gmail.com>
2024-04-08 12:51:52 +0800

    update: initial code for  space, spaces, campaign api handler

---
+++ internal/routers/campaign.go
@@ -0,0 +1,24 @@
+package routers
+
+import (
+	"api-service/internal/handler"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	group_Campaign = append(group_Campaign, func(rg *gin.RouterGroup) {
+		campaignGroup(rg, handler.NewCampaignHandler())
+	})
+}
+
+func campaignGroup(rg *gin.RouterGroup, h handler.ICampaignHandler) {
+	// use jwt handler here
+	rg.Use(func(c *gin.Context) {})
+
+	//
+	rg.POST("/")
+
+	//
+	rg.POST("/query", h.Query)
+}
---
+++ internal/routers/spaces.go
@@ -0,0 +1,26 @@
+package routers
+
+import (
+	"api-service/internal/handler"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	group_Spaces = append(group_Spaces, func(rg *gin.RouterGroup) {
+		spacesGroup(rg, handler.NewSpacesHandler())
+	})
+}
+
+func spacesGroup(rg *gin.RouterGroup, h handler.ISpacesHandler) {
+	// use jwt handler here
+	rg.Use(func(c *gin.Context) {})
+
+	//
+	rg.POST("/", h.Create)
+
+	rg.POST("/query", h.Query)
+
+	rg.POST("/follow", h.Follow)
+
+	rg.POST("/unfollow", h.UnFollow)
+}
